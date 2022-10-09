750497db6c95cee8bb02e0fb789dd7d084048af0
Volos Ivan <ivan.volos1@gmail.com>
2022-10-09 16:46:17 +0300

    Added an angular practice project for different rxjs/scss/ts practices

---
+++ angular-dependency-injection/angular-practice/src/app/features/spinner/spinner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'di-spinner',
+  templateUrl: './spinner.component.html',
+  styles: ['./spinner.component.scss']
+})
+export class SpinnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular-dependency-injection/angular-practice/src/app/shared/shell/shell.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
+import {map, Observable, shareReplay} from "rxjs";
+
+@Component({
+  selector: 'di-shell',
+  templateUrl: './shell.component.html',
+  styles: [
+  ]
+})
+export class ShellComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Handset])
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+}
